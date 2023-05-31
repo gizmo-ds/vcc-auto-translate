6d8845a3dcdf65cefdb726208b47a2704a8e05bf
Gizmo <cunyu@liuli.lol>
2023-05-31 10:12:22 +0800

    Revert "refactor: hash localization"
    This reverts commit fc7660412ff5061c706126248d79f8bdd4dedd28.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { text_hash } from './utils'
-
 const supportedLanguages = ['zh-CN', 'zh-TW']
 
 const language = navigator.language
-const text_record: Record<string, string> = {}
 
 if (supportedLanguages.includes(language)) start(language)
 
@@ -11,10 +8,10 @@ async function start(language: string) {
   const tr = (await fetch(`/localization/${language}.json`).then((resp) =>
     resp.json()
   )) as Record<string, string>
-  let observer = new MutationObserver(async (mutations) => {
-    for (const mutation of mutations) {
-      await vcc_auto_translate(mutation.target as HTMLElement, tr)
-    }
+  let observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      vcc_auto_translate(mutation.target as HTMLElement, tr)
+    })
   })
   observer.observe(document.querySelector('#root')!, {
     childList: true,
@@ -22,10 +19,7 @@ async function start(language: string) {
   })
 }
 
-async function vcc_auto_translate(
-  node: HTMLElement,
-  tr: Record<string, string>
-) {
+function vcc_auto_translate(node: HTMLElement, tr: Record<string, string>) {
   const selectors = [
     '.fui-Button',
     '.fui-Title1, .fui-Title3, .fui-Subtitle1, .fui-Subtitle2',
@@ -36,32 +30,29 @@ async function vcc_auto_translate(
     '.list-disc>li',
     '.fui-Alert',
   ]
-  for (const selector of selectors) {
-    for (const e of node.querySelectorAll(selector)) {
-      for (const child of e.childNodes) {
+  selectors.forEach((selector) => {
+    node.querySelectorAll(selector).forEach((e) => {
+      e.childNodes.forEach((child) => {
         const text = child.textContent
-        if (!text) continue
-        if (!text_record[text]) text_record[text] = await text_hash(text)
-
-        if (tr[text_record[text]]) {
+        if (text) {
+          text_record[text] = ''
+        }
+        if (text && tr[text]) {
           if (
             text === 'New Project' &&
             (child as HTMLElement).classList &&
             !(child as HTMLElement).classList.contains('fui-Title1')
           )
             return
-          child.textContent = tr[text_record[text]]
+          child.textContent = tr[text]
           if (text === 'Projects')
             (e as HTMLElement).style.whiteSpace = 'nowrap'
         }
-      }
-    }
-  }
-  for (const e of node.querySelectorAll('[placeholder]')) {
+      })
+    })
+  })
+  node.querySelectorAll('[placeholder]').forEach((e) => {
     const text = e.getAttribute('placeholder')
-    if (!text) continue
-    if (!text_record[text]) text_record[text] = await text_hash(text)
-    if (tr[text_record[text]])
-      e.setAttribute('placeholder', tr[text_record[text]])
-  }
+    if (text && tr[text]) e.setAttribute('placeholder', tr[text])
+  })
 }
--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export async function text_hash(text: string) {
-  const data = new TextEncoder().encode(text)
-  const hash = await crypto.subtle.digest('SHA-1', data)
-  const hex = Array.from(new Uint8Array(hash))
-    .map((byte) => byte.toString(16).padStart(2, '0'))
-    .join('')
-  return hex
-}
