75f2474253dd373c646c3ff470cbbae287e115ae
Misaka-L <lipww1234@foxmail.com>
2024-03-17 15:07:35 +0800

    chore: better loading tips ui

--- src/script-loader.ts
+++ src/script-loader.ts
@@ -4,6 +4,8 @@ import { Translater } from './translate'
 import zhHans from '../localization/zh-hans.json'
 import zhHant from '../localization/zh-hant.json'
 
+import { fluentProgress, provideFluentDesignSystem } from '@fluentui/web-components'
+
 const store = createStore('vcc_auto_translate', 'store')
 
 main()
@@ -28,11 +30,13 @@ async function main() {
     u.pathname = index_script_file
     const code = await fetch(u.href).then((res) => res.text())
     patched_code = await injector(code, 'vcc_auto_translate')
-    loading.innerText = '翻译补丁已应用 🎉'
+
+    update_loading(loading, '翻译补丁已应用 🎉')
+
     kv_set('patched-filename', patched_filename, store)
     await kv_set('patched-content', patched_code, store)
 
-    setTimeout(() => loading.remove(), 1000)
+    setTimeout(() => loading.remove(), 2000)
   }
 
   const translater = new Translater(localization)
@@ -50,12 +54,100 @@ async function load_patched_code(patched_code?: string) {
 }
 
 function create_loading(text = '正在应用翻译补丁...') {
-  const loading = document.createElement('p')
-  loading.innerText = text
-  loading.style.height = '100vh'
-  loading.style.textAlign = 'center'
-  loading.style.paddingTop = '45vh'
-  loading.style.fontSize = 'xxx-large'
-  loading.style.zIndex = '2000'
+  provideFluentDesignSystem().register(fluentProgress())
+
+  const loading = document.createElement('div')
+
+  loading.className = 'patch-loading-cover'
+
+  const loadingText = document.createElement('p')
+  loadingText.className = 'patch-loading-text'
+  loadingText.innerText = text
+
+  const loadingProgress = document.createElement('fluent-progress')
+  loadingProgress.className = 'patch-loading-progress'
+  loadingProgress.setAttribute('indeterminate', '')
+
+  const loadingStyle = document.createElement('style')
+  loadingStyle.innerHTML = `
+  .patch-loading-cover {
+    --patch-loading-cover-dark-background: #1f1f1f;
+    --patch-loading-cover-dark-color: #ffffff;
+
+    --patch-loading-cover-light-background: #fafafa;
+    --patch-loading-cover-light-color: #242424;
+  }
+
+  .patch-loading-cover {
+      background: var(--patch-loading-cover-light-background);
+      color: var(--patch-loading-cover-light-color) !important;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    .patch-loading-cover {
+      background: var(--patch-loading-cover-dark-background);
+      color: var(--patch-loading-cover-dark-color) !important;
+    }
+  }
+  
+  .patch-loading-cover.dark {
+    background: var(--patch-loading-cover-dark-background) !important;
+    color: var(--patch-loading-cover-dark-color) !important;
+  }
+
+  .patch-loading-cover.light {
+      background: var(--patch-loading-cover-light-background);
+      color: var(--patch-loading-cover-light-color) !important;
+  }
+
+  .patch-loading-cover {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    height: 100vh;
+    width: 100vw;
+
+    z-index: 1000;
+  }
+
+  .patch-loading-text {
+    font-size: xx-large;
+  }
+
+  .patch-loading-progress {
+    margin-top: 1rem;
+    width: 400px;
+  }
+  `
+
+  document.head.appendChild(loadingStyle)
+
+  loading.appendChild(loadingText)
+  loading.appendChild(loadingProgress)
+
+  switch (localStorage.getItem('app_theme')) {
+    case 'Light':
+      loading.classList.add('light')
+      break
+    case 'Dark':
+      loading.classList.add('dark')
+      break
+  }
+
   return loading
+}
+
+function update_loading(loadingElement: HTMLElement, text: string) {
+  const loadingText = document.createElement('p')
+  loadingText.className = 'patch-loading-text'
+  loadingText.innerText = text
+
+  loadingElement.innerHTML = ''
+  loadingElement.appendChild(loadingText)
 }
